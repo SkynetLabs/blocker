09f11148629cb5694bd86ea87f368ff3bfcfe3c0
Ivaylo Novakov <inovakov@gmail.com>
2021-12-06 11:17:39 +0100

    Temporarily add a local version of ExtractSkylinkHash until we update it in accounts. This one supports base32 skylinks.

--- api/handlers.go
+++ api/handlers.go
@@ -3,10 +3,10 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/SkynetLabs/blocker/database"
-	accdb "github.com/SkynetLabs/skynet-accounts/database"
 	"github.com/julienschmidt/httprouter"
 	"gitlab.com/NebulousLabs/errors"
 	skyapi "gitlab.com/SkynetLabs/skyd/node/api"
@@ -40,7 +40,7 @@ func (api *API) blockPOST(w http.ResponseWriter, r *http.Request, _ httprouter.P
 		skyapi.WriteError(w, skyapi.Error{err.Error()}, http.StatusBadRequest)
 		return
 	}
-	body.Skylink, err = accdb.ExtractSkylinkHash(body.Skylink)
+	body.Skylink, err = extractSkylinkHash(body.Skylink)
 	if err != nil {
 		skyapi.WriteError(w, skyapi.Error{errors.AddContext(err, "invalid skylink provided").Error()}, http.StatusBadRequest)
 		return
@@ -74,4 +74,15 @@ func (api *API) blockPOST(w http.ResponseWriter, r *http.Request, _ httprouter.P
 	}
 	api.staticLogger.Debugf("Added skylink %s", skylink.Skylink)
 	skyapi.WriteSuccess(w)
+}
+
+// extractSkylinkHash extracts the skylink hash from the given skylink that might
+// have protocol, path, etc. within it.
+func extractSkylinkHash(skylink string) (string, error) {
+	extractSkylinkRE := regexp.MustCompile("^.*([a-z0-9]{55})|([a-zA-Z0-9-_]{46}).*$")
+	m := extractSkylinkRE.FindStringSubmatch(skylink)
+	if len(m) < 2 {
+		return "", errors.New("no valid skylink found in string " + skylink)
+	}
+	return m[1], nil
 }
